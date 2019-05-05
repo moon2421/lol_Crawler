2e2670c37cfcd960799f91fb495ce76f574a4e33
minkinew <44183457+minkinew@users.noreply.github.com>
2019-05-05 22:36:29 +0900

    service패키지의 LeagueService클래스 추가

--- src/main/java/kr/ac/cnu/team16/service/LeagueService.java
+++ src/main/java/kr/ac/cnu/team16/service/LeagueService.java
@@ -1,5 +1,50 @@
 package kr.ac.cnu.team16.service;
 
+import kr.ac.cnu.team16.api.LeagueApi;
+import kr.ac.cnu.team16.domain.LeaguePositionDTO;
+import kr.ac.cnu.team16.repository.LeagueRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@Service
 public class LeagueService {
-  
+    @Autowired
+    private LeagueApi leagueApi;
+    @Autowired
+    private LeagueRepo leagueRepo;
+
+    public Set<LinkedHashMap> getLeagueId(String id) {
+        Set<LinkedHashMap> leagueSet = leagueApi.getLeague(id);
+        Iterator<LinkedHashMap> iterator = leagueSet.iterator();
+        LinkedHashMap<String, Object> leageHashMap;
+        while (iterator.hasNext()) {
+            leageHashMap = iterator.next();
+            LeaguePositionDTO leaguePositionDTO = new LeaguePositionDTO();
+
+            leaguePositionDTO.setWins((int) leageHashMap.get("wins"));
+            leaguePositionDTO.setLosses((int) leageHashMap.get("losses"));
+            leaguePositionDTO.setLeaguePoints((int) leageHashMap.get("leaguePoints"));
+
+            leaguePositionDTO.setSummonerId((String) leageHashMap.get("summonerId"));
+            leaguePositionDTO.setRank((String) leageHashMap.get("rank"));
+            leaguePositionDTO.setTier((String) leageHashMap.get("tier"));
+            leaguePositionDTO.setLeagueName((String) leageHashMap.get("leagueName"));
+            leaguePositionDTO.setSummonerName((String) leageHashMap.get("summonerName"));
+            leaguePositionDTO.setQueueType((String) leageHashMap.get("queueType"));
+            leaguePositionDTO.setLeagueId((String) leageHashMap.get("leagueId"));
+
+            leaguePositionDTO.setHotStreak((boolean) leageHashMap.get("hotStreak"));
+            leaguePositionDTO.setVeteran((boolean) leageHashMap.get("veteran"));
+            leaguePositionDTO.setInactive((boolean) leageHashMap.get("inactive"));
+            leaguePositionDTO.setFreshBlood((boolean) leageHashMap.get("freshBlood"));
+
+            leagueRepo.insertData(leaguePositionDTO);
+        }
+
+        return leagueSet;
+    }
+
 }
