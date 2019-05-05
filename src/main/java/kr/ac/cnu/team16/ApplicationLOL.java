cf0d1ec534c2afd1ecaf16601f6e085ecf66c842
moon2421 <sikgakminwo@hanmail.net>
2019-05-05 22:19:14 +0900

    lolapi skeleton

---
+++ src/main/java/kr/ac/cnu/team16/ApplicationLOL.java
@@ -0,0 +1,12 @@
+package kr.ac.cnu.team16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLOL {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLOL.class, args);
+	}
+}
