6e2cfdd3bae7e3985e79b58cececc1ede6b503f1
tmdals5718 <48197211+tmdals5718@users.noreply.github.com>
2019-05-05 22:56:20 +0900

    LeagueRepo  mongoTemplete.insert 추가

--- src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
+++ src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
@@ -1,11 +1,17 @@
 package kr.ac.cnu.team16.repository;
-import org.springframework.stereotype.Repository;
+
+import kr.ac.cnu.team16.domain.LeaguePositionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class LeagueRepo {
     @Autowired
     private MongoTemplate mongoTemplete;
 
+    public void insertData(LeaguePositionDTO leaguePositionDTO){ 
+        mongoTemplete.insert(leaguePositionDTO);
+    }
 }
