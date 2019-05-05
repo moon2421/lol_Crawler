34d485460dec1bab5465c888814f12b812ab9640
moon2421 <47843410+moon2421@users.noreply.github.com>
2019-05-05 22:35:04 +0900

    Riot Controller 내용 추가합니다.

--- src/main/java/kr/ac/cnu/team16/controller/RiotController.java
+++ src/main/java/kr/ac/cnu/team16/controller/RiotController.java
@@ -1,5 +1,42 @@
+
 package kr.ac.cnu.team16.controller;
 
+import kr.ac.cnu.team16.domain.SummonerDTO;
+import kr.ac.cnu.team16.service.LeagueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@RestController
 public class RiotController {
 
+    @Autowired
+    private LeagueService leagueService;
+    @Autowired
+    private LeagueService.SummonerService summonerService;
+
+    @GetMapping("/leagues/{name}")
+    public Set<LinkedHashMap> getLeagueName(@PathVariable String name) {
+        SummonerDTO summonerDTO = summonerService.getSummonerByName(name);
+        String id = summonerDTO.getId();
+        return leagueService.getLeagueId(id);
+    }
+
+    @RestController
+    public static class SummonerController {
+
+        @Autowired
+        private LeagueService.SummonerService summonerService;
+
+        @GetMapping("/summoners/{name}")
+        public SummonerDTO getSummonerName(@PathVariable String name) {
+            return summonerService.getSummonerByName(name);
+        }
+
+
+    }
 }
