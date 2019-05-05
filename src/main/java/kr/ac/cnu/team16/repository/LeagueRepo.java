afa78bac8f6c0cb6a96b5876f4f13634c147eae1
tmdals5718 <48197211+tmdals5718@users.noreply.github.com>
2019-05-05 22:38:17 +0900

    LeagueRepo클래스 내용추가
    @Repository + @Autowired

--- src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
+++ src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
@@ -1,5 +1,10 @@
 ﻿package kr.ac.cnu.team16.repository;
 
-public class LeagueRepo {
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+@Repository
+public class LeagueRepo {
+  private MongoTemplate mongoTemplete;
 }
