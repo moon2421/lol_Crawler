b4766163934aa30835c262befbb47d2b27ba652b
tmdals5718 <48197211+tmdals5718@users.noreply.github.com>
2019-05-05 22:45:01 +0900

    LeagueRepo 내용추가
    import kr.ac.cnu.team16.domain.LeaguePositionDTO; 추가

--- src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
+++ src/main/java/kr/ac/cnu/team16/repository/LeagueRepo.java
@@ -1,5 +1,6 @@
 ﻿package kr.ac.cnu.team16.repository;
 
+import kr.ac.cnu.team16.domain.LeaguePositionDTO;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
