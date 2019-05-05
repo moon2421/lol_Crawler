81d27fea7483625ea5f98d7fac15aa4164ad17d1
tmdals5718 <48197211+tmdals5718@users.noreply.github.com>
2019-05-05 22:26:21 +0900

    LeaguePositionDTO클래스 내용추가
    LeaguePositionDTO클래스 내용추가합니다.

--- src/main/java/kr/ac/cnu/team16/domain/LeaguePositionDTO.java
+++ src/main/java/kr/ac/cnu/team16/domain/LeaguePositionDTO.java
@@ -4,5 +4,18 @@ import lombok.Data;
 
 @Data
 public class LeaguePositionDTO {
-
+    private int wins;
+    private int losses;
+    private int leaguePoints;
+    private String rank;
+    private String tier;
+    private String leagueName;
+    private String summonerName;
+    private String queueType;
+    private boolean hotStreak;
+    private boolean veteran;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String summonerId;
 }
