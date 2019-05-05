2e896033a7dee537c1fac2c3ec1711aed6fa499b
tmdals5718 <48197211+tmdals5718@users.noreply.github.com>
2019-05-05 22:29:26 +0900

    SummonerDTO클래스 내용추가
    SummonerDTO클래스 내용추가합니다

--- src/main/java/kr/ac/cnu/team16/domain/SummonerDTO.java
+++ src/main/java/kr/ac/cnu/team16/domain/SummonerDTO.java
@@ -4,5 +4,11 @@ import lombok.Data;
 
 @Data
 public class SummonerDTO {
-
+   private int profileIconId;
+    private String name;
+    private String puuid;
+    private long summonerLevel;
+    private String id; // 이거 뽑아써야 됨.
+    private String accountId;
+    private long revisionDate;
 }
